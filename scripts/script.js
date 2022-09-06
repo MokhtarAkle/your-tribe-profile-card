47a63fcae4f095d92ce14c5635d94353ceed9832
MokhtarAkle <mokhtar.akle@hva.nl>
2022-09-06 13:01:00 +0200

    dag 2 les progress

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,43 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
 
-function expand () {
-  document.body.classList.toggle('expand')
+var showButton = document.querySelector("#showCards");
+var frontAnimation = document.querySelector(".animationFront");
+var middleAnimation = document.querySelector(".animationMiddle");
+var rightAnimation = document.querySelector(".animationRight");
+
+
+
+function testExpand(){
+  if(frontAnimation.classList.contains("animationFront")){
+    frontAnimation.classList.remove("animationFront");
+    frontAnimation.classList.add("frontTransformation");
+  }
+  else{
+    frontAnimation.classList.remove("frontTransformation");
+    frontAnimation.classList.add("animationFront");
+  }
+
+  if(middleAnimation.classList.contains("animationMiddle")){
+    middleAnimation.classList.remove("animationMiddle");
+    middleAnimation.classList.add("middleTransformation");
+  }
+  else{
+    middleAnimation.classList.remove("middleTransformation");
+    middleAnimation.classList.add("animationMiddle");
+  }
+
+  if(rightAnimation.classList.contains("animationRight")){
+    rightAnimation.classList.remove("animationRight");
+    rightAnimation.classList.add("rightTransformation");
+  }
+  else{
+    rightAnimation.classList.remove("rightTransformation");
+    rightAnimation.classList.add("animationRight");
+  }
+
 }
+
+showButton.addEventListener("click", testExpand);
+
+
+
+
