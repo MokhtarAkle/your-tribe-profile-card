5b53c41d7178f3c7e8afcce00419f34c938a753d
MokhtarAkle <mokhtar.akle@hva.nl>
2022-09-22 18:17:31 +0200

    maybe progress

--- scripts/script.js
+++ scripts/script.js
@@ -4,62 +4,56 @@ var frontAnimation = document.querySelector("#animationFront");
 var middleAnimation = document.querySelector("#animationMiddle");
 var rightAnimation = document.querySelector("#animationRight");
 var flipButton = document.querySelector("#showCards");
+var frontBackanim = document.querySelector("#frontBackside");
 
 
 
 function testExpand(){
   if(frontAnimation.classList.contains("check") || frontAnimation.classList.contains("frontTransformationFlip")){ /* add class with styling for backside when available */
-    frontAnimation.classList.remove("frontTransformationBack");
-    frontAnimation.classList.remove("frontTransformationFlip");
-    frontAnimation.classList.remove("check");
-    frontAnimation.classList.remove("frontTransformationFlipB");
+    frontAnimation.classList.remove("frontTransformationBack", "frontTransformationFlip", "check", "frontTransformationFlipB");
     frontAnimation.classList.toggle("frontTransformation");
     flipButton.disabled = true;
 
   }
   else{
     frontAnimation.classList.toggle("frontTransformation");
-    frontAnimation.classList.add("frontTransformationBack");
-    frontAnimation.classList.add("check");
+    frontAnimation.classList.add("frontTransformationBack", "check");
     flipButton.disabled = false;
   }
 
   if(middleAnimation.classList.contains("check")){
-    middleAnimation.classList.remove("middleTransformationBack");
-    middleAnimation.classList.remove("check");
+    middleAnimation.classList.remove("middleTransformationBack", "check");
     middleAnimation.classList.toggle("middleTransformation");
   }
   else{
     middleAnimation.classList.toggle("middleTransformation");
-    middleAnimation.classList.add("middleTransformationBack");
-    middleAnimation.classList.add("check");
+    middleAnimation.classList.add("middleTransformationBack", "check");
   }
 
   if(rightAnimation.classList.contains("check")){
-    rightAnimation.classList.remove("rightTransformationBack");
-    rightAnimation.classList.remove("check");
+    rightAnimation.classList.remove("rightTransformationBack", "check");
     rightAnimation.classList.toggle("rightTransformation");
   }
   else{
     rightAnimation.classList.toggle("rightTransformation");
-    rightAnimation.classList.add("rightTransformationBack");
-    rightAnimation.classList.add("check");
+    rightAnimation.classList.add("rightTransformationBack", "check");
   }
 
 }
 
 function testFlip(){
   if(frontAnimation.classList.contains("check")){
-    frontAnimation.classList.remove("frontTransformationFlipB");
-    frontAnimation.classList.remove("check");
+    frontAnimation.classList.remove("frontTransformationFlipB", "check");
     frontAnimation.classList.toggle("frontTransformationFlip")
     setTimeout(testFlipChange, 2000);
+    frontBackanim.style.opacity = "1";
   }
   else{
     frontAnimation.classList.toggle("frontTransformationFlip");
     frontAnimation.classList.remove("frontTransformationBack");
-    frontAnimation.classList.add("frontTransformationFlipB");
-    frontAnimation.classList.add("check");
+    frontAnimation.classList.add("frontTransformationFlipB", "check");
+    frontBackanim.style.opacity = "0";
+
   }
 }
 
